6fda6ade17a763fc5aebe4a975cb3acf9654aae8
Muhammetkocaman <kocamamm@gmail.com>
2024-10-24 18:54:59 +0300

    added delegates,uipickerview edited.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,22 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    var delegate : CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String)  {
+        let urlString = "\(baseURL)/\(currency)?\(apiKey)"
+        print(urlString)
+    }
 }
+
